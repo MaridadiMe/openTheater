4d30170e2b70e725da98f39ada0f1383b3f7c477
Norbert Seho <norbertseho@gmail.com>
2022-03-15 00:59:06 +0300

    edited title in index page

--- theater/views.py
+++ theater/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    # returns the index page which bootstaps angular app
+    # front end is a single page app created in angular
     return render(request, 'theater/index.html')
