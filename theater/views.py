1ee5a2dc99cde9e5b269fd05359c9bf8428dd0bf
Norbert Seho <norbertseho@gmail.com>
2022-03-15 00:25:17 +0300

    added static folder

--- theater/views.py
+++ theater/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('first time for a movie boy')
+    return render(request, 'theater/index.html')
