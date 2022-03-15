3e92e7971e22e7531e60f76b9ea67ee3c17c7256
Norbert Seho <norbertseho@gmail.com>
2022-03-16 00:16:14 +0300

    added categoryserializer class, categorylist view and an api to get categories

--- theater/views.py
+++ theater/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . serializers import CategorySerializer
+from . models import Category
 
 # Create your views here.
 
@@ -7,3 +14,12 @@ def index(request):
     # returns the index page which bootstaps angular app
     # front end is a single page app created in angular
     return render(request, 'theater/index.html')
+
+
+class  CategoryList(APIView):
+    
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return JsonResponse(serializer.data, safe=False)
+        
